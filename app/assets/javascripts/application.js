b2c1323c674519b2bcd62e6e4b273860ec23d5ec
Catherine Meyers <cmeyers@mavenlink.com>
2016-10-14 17:39:00 -0700

    Starts adding webpack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-//= require materialize.min
+// Looking for this app's javascript?
+// You're barking up the wrong tree! (no pun intended)
+// I broke up with the asset pipeline and now use webpack to compile assets
+// All javascript files are in `app/frontend/javascripts/`
+// and are compiled into `bundle.js`
+// which is thrown into `app/assets/javascripts/` when you run webpack
--- app/frontend/javascripts/index.js
+++ app/frontend/javascripts/index.js
@@ -1,3 +1,5 @@
+require('expose?$!expose?jQuery!jquery');
+
 $(document).ready(function() {
   $('.doctors-index-btn').on('click', function(e) {
     e.preventDefault();
@@ -6,7 +8,7 @@ $(document).ready(function() {
       dataType: 'json',
       url: '/api/v1/doctors'
     }).done(function(response) {
-      console.log('done');
+      console.log('here are your doctors');
       console.log(response);
     }).fail(function(response) {
       console.log(response);
